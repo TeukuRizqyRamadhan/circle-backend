a43c24bf3dfbab5cea06f4e36e40bc6897d83057
Sundus <pcinta0@gmail.com>
2024-05-14 16:25:26 +0700

    prisma init

--- server.ts
+++ server.ts
@@ -1,15 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import db from "./src/lib/db";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
 app.get("/", (req, res) => {
    res.send("CIRCLE APP - API");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+   await db.$connect();
    console.log("Server is running at port " + PORT);
 });
